e88733aaf3fda3ea39ce92441591c84b9263619f
Helena Pennanen <m.helena.pennanen@gmail.com>
2018-07-08 16:10:14 +0300

    2.8 puhelinluettelo osa 3 --- versio 1

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -4,28 +4,62 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      persons: [{ name: "Arto Hellas" }],
-      newName: ""
+      persons: [
+        {
+          name: "Arto Hellas",
+          number: "1234567"
+        },
+        {
+          name: "Helena",
+          number: "1234567"
+        }
+      ],
+      newPerson: {
+        name: "",
+        number: ""
+      }
     };
+    this.addNumber = this.addNumber.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   addNumber = event => {
     event.preventDefault();
     const names = this.state.persons.map(person => person.name);
-    if (names.indexOf(this.state.newName) === -1) {
-      const person = {
-        name: this.state.newName
-      };
+    if (names.indexOf(this.state.name) === -1) {
+      const person = this.state.newPerson;
       const persons = this.state.persons.concat(person);
+      const newPerson = {
+        name: "",
+        number: ""
+      };
       this.setState({
         persons: persons,
-        newName: ""
+        newPerson: newPerson
       });
     }
   };
 
   handleChange = event => {
-    this.setState({ newName: event.target.value });
+    if (event.target.name === "name") {
+      const uusi = {
+        name: event.target.value,
+        number: this.state.newPerson.number
+      };
+
+      this.setState({
+        newPerson: uusi
+      });
+    } else {
+      const uusi2 = {
+        name: this.state.newPerson.name,
+        number: event.target.value
+      };
+
+      this.setState({
+        newPerson: uusi2
+      });
+    }
   };
 
   render() {
@@ -33,29 +67,40 @@ class App extends React.Component {
       <div>
         <h2>Puhelinluettelo</h2>
 
-        <form onSubmit={this.addNumber} onChange={this.handleChange}>
+        <form onSubmit={this.addNumber}>
           <div>
-            nimi: <input value={this.state.newName} />
+            nimi:
+            <input
+              name="name"
+              id="name"
+              value={this.state.newPerson.name}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            numero:
+            <input
+              name="number"
+              id="number"
+              value={this.state.newPerson.number}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
+
         <h2>Numerot</h2>
-        {this.state.persons.map(person => (
-          <Person key={person.name} person={person} />
+        {this.state.persons.map(p => (
+          <p key={p.name}>
+            {" "}
+            {p.name} {p.number}
+          </p>
         ))}
       </div>
     );
   }
 }
-
-const Person = ({ person }) => {
-  return (
-    <div>
-      <p>{person.name}</p>
-    </div>
-  );
-};
 
 export default App;

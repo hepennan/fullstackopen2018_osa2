f771222ce9983a440106e3f16b49d13d1795b1f0
Helena Pennanen <m.helena.pennanen@gmail.com>
2018-07-08 12:08:18 +0300

    2.6 puhelinluettelo osa 1

---
+++ puhelinluettelo/src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [{ name: "Arto Hellas" }],
+      newName: ""
+    };
+  }
+
+  addNumber = event => {
+    event.preventDefault();
+    const person = {
+      name: this.state.newName
+    };
+    const persons = this.state.persons.concat(person);
+    this.setState({
+      persons: persons,
+      newName: ""
+    });
+  };
+
+  handleChange = event => {
+    this.setState({ newName: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+
+        <form onSubmit={this.addNumber} onChange={this.handleChange}>
+          <div>
+            nimi: <input value={this.state.newName} />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        {this.state.persons.map(person => (
+          <Person key={person.name} person={person} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const Person = ({ person }) => {
+  return (
+    <div>
+      <p>{person.name}</p>
+    </div>
+  );
+};
+
+export default App;
